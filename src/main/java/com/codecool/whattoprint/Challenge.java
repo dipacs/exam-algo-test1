d6daebb7e0e4aac4c82ed7ef97ac19d8a4435709
David Ipacs <david.ipacs@gmail.com>
2021-03-22 16:20:05 +0100

    Initial commit

---
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -0,0 +1,8 @@
+package com.codecool.whattoprint;
+
+class Challenge {
+    public static String decideAndSay(String[] words, int number) {
+        //TODO your code comes here
+        return null;
+    }
+}
---
+++ src/test/java/com/codecool/whattoprint/SayCodeCoolTests.java
@@ -0,0 +1,35 @@
+package com.codecool.whattoprint;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SayCodeCoolTests {
+    private final static String[] words = new String[]{"Code", "Cool", "Apple"};
+
+    @Test
+    public void testCode() {
+        assertEquals("Code", Challenge.decideAndSay(words, 3));
+    }
+
+    @Test
+    public void testCool() {
+        assertEquals("Cool", Challenge.decideAndSay(words, 7));
+    }
+
+    @Test
+    public void testCodeCool() {
+        assertEquals("CodeCool", Challenge.decideAndSay(words, 21));
+    }
+
+    @Test
+    public void testEmpthyString() {
+        assertEquals("", Challenge.decideAndSay(words, 10));
+    }
+
+    @Test
+    public void testNegative() {
+        assertEquals("Cool", Challenge.decideAndSay(words, -7));
+    }
+}
